6165fd8d674b6a3dedfee2cc03936a21dc72754c
Carl-Adam Mattsson <mattsada@gmail.com>
2021-09-09 11:38:45 -0700

    Delete install_dep.R

--- install_dep.R
+++
@@ -1,7 +0,0 @@
-#dependencies
-install.packages("stringr")
-install.packages("dplyr")
-install.packages("knitr")
-install.packages("gtsummary")
-install.packages("remotes")
-remotes::install_github("rstudio/gt")
