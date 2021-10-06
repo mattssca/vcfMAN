6cbe7b7eebf93a092320513242adc54cb26d099e
Carl-Adam Mattsson <mattsada@gmail.com>
2021-10-05 17:55:57 -0700

    Update img_man_structuralvariants.sh
    format first table spacing

--- scripts/img_man_structuralvariants.sh
+++ scripts/img_man_structuralvariants.sh
@@ -2,7 +2,7 @@
 
 #combine png for report format
 convert out/SVs/figs/*_box1.png out/SVs/figs/*_SV_summary.png out/SVs/figs/*04_sum_genotypes.png out/SVs/figs/*_box1.png +append -quiet out/SVs/out1.png
-convert out/SVs/figs/*_header.png out/SVs/out1.png out/SVs/figs/*_box2.png -append -quiet out/SVs/out1.png
+convert out/SVs/figs/*_header.png out/SVs/figs/*_box2.png out/SVs/out1.png out/SVs/figs/*_box2.png -append -quiet out/SVs/out1.png
 convert -quiet out/SVs/figs/*02_sv_size_violin.png out/SVs/figs/*03_sv_binned.png +append out/SVs/out2.png
 convert -density 300x300 -units pixelsperinch out/SVs/figs/*_circos.pdf -background white -alpha background -alpha off -compress zip +adjoin out/SVs/figs/circos.png
 convert -quiet out/SVs/out1.png out/SVs/figs/*_box2.png out/SVs/figs/*01_sv_chrdist.png out/SVs/figs/*_box2.png out/SVs/out2.png out/SVs/figs/*_box2.png out/SVs/figs/circos.png out/SVs/figs/*_box2.png -append out/SVs/report.png
